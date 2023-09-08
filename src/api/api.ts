bdb10b6827caaf06852b7b3f2f94f00f9faed9b2
Deorren Erive <erive92d@gmail.com>
2023-09-08 05:33:51 -0700

    s

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,27 +1,19 @@
 import axios from "axios";
+const options = {
+  headers: {
+    accept: 'application/json',
+    Authorization: import.meta.env.VITE_AUTH || process.env.AUTH
+  }
+};
 
 export const initialPage = async (category:string, page:number) => {
     const url =  `https://api.themoviedb.org/3/movie/${category}?language=en-US&page=${page}`;
-      const options = {
-        headers: {
-          accept: 'application/json',
-          Authorization: import.meta.env.VITE_AUTH || process.env.VITE_AUTH
-        }
-      };
-
      const response = await axios.get(url, options) 
      return response.data.results
 }
 
 export const viewMovie = async (movieId:string) => {
     const url = `https://api.themoviedb.org/3/movie/${movieId}`;
-      const options = {
-        headers: {
-          accept: 'application/json',
-          Authorization: import.meta.env.VITE_AUTH || process.env.VITE_AUTH
-        }
-      };
-
      const response = await axios.get(url, options) 
     return response.data
 }
