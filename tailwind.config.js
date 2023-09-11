79c6886f69e70d08c74167aa96f8367079ccd00c
Deorren Erive <erive92d@gmail.com>
2023-09-11 16:12:40 -0700

    s

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ]
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   // eslint-disable-next-line no-undef
   plugins: [require("daisyui")],
-    daisyui: {
+  daisyui: {
     styled: true,
     themes: ["cupcake", "dracula"],
     base: true,
