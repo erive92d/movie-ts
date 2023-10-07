b44a0c0450fc959592a1fc8a5e62172138256ce4
Deorren Erive <mac@Deorrens-MacBook-Air.local>
2023-10-07 16:40:53 -0700

    s

--- src/components/SearchComps/Dropdown.tsx
+++ src/components/SearchComps/Dropdown.tsx
@@ -9,7 +9,7 @@ export default function Dropdown({ options, handleFormat, selectedFormat }: Drop
 
     return (
         <div className=''>
-            <select className="select max-w-xs text-black duration-200" onChange={handleFormat} value={selectedFormat}>
+            <select className="select select-ghost rounded-r-none max-w-xs text-gray-400 duration-200" onChange={handleFormat} value={selectedFormat}>
                 {options && options.map((opt, index) => (
                     <option onClick={() => handleFormat} value={opt} key={index}>
                         <h1 className=''>{opt.toUpperCase()}</h1>
--- src/pages/Movies/Trending.tsx
+++ src/pages/Movies/Trending.tsx
@@ -11,12 +11,12 @@ export default function Trending() {
 
     const [currentDisplay, setCurrentDisplay] = useState<'movie' | 'tv' | string>('movie')
 
-    const {debouncedValue, loading} = UseDebounce(currentDisplay, 1000)
+    const { debouncedValue, loading } = UseDebounce(currentDisplay, 1000)
 
-    const {data } = useQuery({
-        queryKey:["trending", debouncedValue],
+    const { data } = useQuery({
+        queryKey: ["trending", debouncedValue],
         queryFn: () => fetchTrending(debouncedValue)
-     
+
     })
 
     const handleTvOrMovie = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
@@ -24,21 +24,21 @@ export default function Trending() {
         setCurrentDisplay(event.currentTarget.name)
     }
 
-  return (
-    <div className="p-2 ">
-        <div className="flex flex-col py-2 gap-1">
-            <div className="flex justify-between items-center">
-                <h1 className="text-3xl  ">Trending</h1>
-                <div>
-                    <button onClick={handleTvOrMovie} name="movie" className={`btn btn-sm duration-200 ${currentDisplay === 'movie' ? "bg-cyan-500 text-white" : ""}`}>Movie</button>
-                    <button onClick={handleTvOrMovie} name="tv" className={`btn btn-sm duration-200 ${currentDisplay === 'tv' ? "bg-cyan-500 text-white" : ""}`}>Tv Show</button>
+    return (
+        <div className="p-2 ">
+            <div className="flex flex-col py-2 gap-1">
+                <div className="flex justify-between items-center">
+                    <h1 className="text-2xl font-thin ">TRENDING {currentDisplay.toUpperCase()}</h1>
+                    <div>
+                        <button onClick={handleTvOrMovie} name="movie" className={`btn btn-sm btn-ghost duration-200 ${currentDisplay === 'movie' ? "bg-cyan-500 text-white" : ""}`}>Movie</button>
+                        <button onClick={handleTvOrMovie} name="tv" className={`btn btn-sm btn-ghost duration-200 ${currentDisplay === 'tv' ? "bg-cyan-500 text-white" : ""}`}>Tv Show</button>
+                    </div>
                 </div>
-            </div>  
-        </div>
-        {data && <ResultCarousel data={data} loading={loading}/>}
-        {/* {currentDisplay === 'movie' ? <Link className="link" to="/feature/movie">See more popular movies</Link> : <Link  className="link" to="/feature/tv">See more popular tv shows</Link>
+            </div>
+            {data && <ResultCarousel data={data} loading={loading} />}
+            {/* {currentDisplay === 'movie' ? <Link className="link" to="/feature/movie">See more popular movies</Link> : <Link  className="link" to="/feature/tv">See more popular tv shows</Link>
  }  */}
-    </div>
-  )
+        </div>
+    )
 }
 
